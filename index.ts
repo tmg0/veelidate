906aefc3c5c11a91d235b7b1f98428680faeb666
zekun.jin <zekun.jin@foxmail.com>
2023-02-19 20:25:27 +0800

    chore: export field validator types

--- index.ts
+++ index.ts
@@ -1,12 +1,14 @@
 import { Validator } from './src/validator'
 import { Field } from './src/field'
 
-export const defineValidator = () => ({
+const defineValidator = () => ({
   setup<T extends Record<string, any>> (getter: () => T) {
     return new Validator(getter)
   }
 })
 
-export const defineField = <T>(value?: T) => {
+const defineField = <T>(value?: T) => {
   return new Field<T>(value)
 }
+
+export { defineValidator, defineField, Validator, Field }
